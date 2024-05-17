14904d07203c2b856777ffa63b1bb4749eec2894
Zachary Proser <zackproser@gmail.com>
2024-05-17 08:33:43 -0400

    Init to win it

--- README.md
+++ README.md
@@ -1,36 +1,80 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Embeddings Demo 
 
-## Getting Started
+### Understand how data is converted to embeddings
 
-First, run the development server:
+![Embeddings demo](./docs/embeddings-demo.png)
+
+### Built With
+
+- Next.js + tailwind
+- OpenAI's `text-embedding-3-small` model
+- Node version 20 or higher
+
+---
+
+### Start the project
+
+**Requires Node version 20+**
+
+From the project root directory, run the following command.
+
+```bash
+npm install
+```
+
+### Set environment variables
+
+Make sure you have populated a `.env.local` file with your OpenAI API key: 
+
+```
+OPENAI_API_KEY="your_openai_api_key_here"
+```
+
+Start the app.
 
 ```bash
 npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Project structure
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
+In this example we opted to use Next.js and the app router, which colocates the frontend and backend code in a single repository.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+**Frontend Client**
 
-## Learn More
+The frontend uses Next.js and tailwind to allow users to enter free form text. This text is split by word on the client-side and then converted to tokens by the `tiktoken` library 
+when the user clicks the `Convert text to embeddings` button. 
 
-To learn more about Next.js, take a look at the following resources:
+OpenAI's `text-embedding-3-small` model extracts the features of the text you provide it and returns vectors representing these features. 
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+**Backend API route**
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+This project exposes an API route: `/api/embeddings`: 
 
-## Deploy on Vercel
+```typescript
+import { NextRequest, NextResponse } from 'next/server';
+import OpenAI from 'openai';
 
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+export async function POST(req: NextRequest) {
+  const { inputText } = await req.json();
+
+  try {
+    const response = await openai.embeddings.create({
+      model: 'text-embedding-3-small',
+      input: inputText,
+    });
+
+    const generatedEmbeddings = response.data[0].embedding;
+    console.log(`Generated embeddings: %o`, generatedEmbeddings);
+
+    return NextResponse.json({ embeddings: generatedEmbeddings }, { status: 200 });
+  } catch (error) {
+    console.error('Error generating embeddings:', error);
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+```
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,108 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { Suspense, useState } from 'react';
+
+const getColorForToken = (token: string) => {
+  const tokenId = token.charCodeAt(0);
+  const hue = (tokenId * 137.508) % 360;
+  return `hsl(${hue}, 50%, 80%)`;
+};
+
+function EmbeddingsDemo() {
+  const [inputText, setInputText] = useState('');
+  const [embeddings, setEmbeddings] = useState<number[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const generateTokens = async () => {
+    try {
+      setError(null);  // Clear any previous errors
+      const response = await fetch('/api/embeddings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ inputText }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to generate embeddings');
+      }
+
+      const data = await response.json();
+      console.log(`data: %o`, data.embeddings);
+      setEmbeddings(data.embeddings);
+    } catch (error) {
+      console.error('Error generating tokens:', error);
+      setError("There was an error generating embeddings. Please try again.")
+    }
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className='flex flex-col items-center justify-center h-full rounded-2xl mt-10 bg-cover text-black'>
+      <div className="backdrop-blur-sm bg-white/30 border-dotted rounded-lg border-2 border-gray-300 p-1 sm:p-5 md:p-12 md:max-w-[90%]">
+        <div className="mx-auto max-w-2xl">
+          <h1 className="text-4xl font-bold mb-8">Embeddings Demo</h1>
+          <p className="mb-6">This interactive demo showcases the process of converting natural language into vectors or embeddings, a fundamental technique used in natural language processing (NLP) and generative AI.</p>
+          <div className="mb-6">
+            <input
+              type="text"
+              id="input-text"
+              name="input-text"
+              onChange={handleInputChange}
+              value={inputText}
+              className="mt-1 block w-full rounded-md bg-white border border-gray-300 placeholder-gray-400 text-black focus:border-[#1C17FF] focus:ring focus:ring-[#1C17FF] focus:ring-opacity-50 p-3 shadow-sm"
             />
-          </a>
+          </div>
+          {error && (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-6" role="alert">
+              <strong className="font-bold">Error: </strong>
+              <span className="block sm:inline">{error}</span>
+            </div>
+          )}
+
+          <div className="mb-6">
+            <p className="text-black mb-6">As you type, your sentence is split into words, the way us humans tend to see and read them:</p>
+            <div className="inline-block">
+              {inputText.split(' ').map((word, index) => (
+                <span
+                  key={index}
+                  className="inline-block px-3 py-2 text-lg font-bold text-white text-shadow"
+                  style={{
+                    backgroundColor: getColorForToken(word),
+                    textShadow: '1px 1px 2px rgba(0, 0, 0, 0.8)',
+                    margin: '0 4px',
+                  }}
+                >
+                  {word}
+                </span>
+              ))}
+            </div>
+            <br />
+            <p className="text-black mt-6 mb-6">But how does a machine understand the defining features of your text? Click the button below to convert your text to embeddings.</p>
+            <button
+              className="bg-[#1C17FF] hover:bg-[#1C17FF]/80 text-white font-bold py-2 px-4 rounded mb-6"
+              onClick={() => generateTokens()}
+            >
+              Convert text to embeddings
+            </button>
+            <div className="overflow-y-scroll h-64">
+              {embeddings}
+            </div>
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+export default function TokenizationDemoWrapper() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <EmbeddingsDemo />
+    </Suspense>
   );
 }
